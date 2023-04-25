db65aeba738839e77522273a9af75677c22e0812
COREcv <core.cvba@gmail.com>
2023-04-25 17:26:28 +0200

    foutje cap eruit

--- dumb.py
+++ dumb.py
@@ -1,13 +1,10 @@
 from matplotlib import pyplot as plt# Dom laden: op elk tijdsstip t vraagt elke auto die wilt laden, max vermogen. Als boven cap: verschil met cap/#ladende auto's aftrekken van geladen vermogen.
 import numpy as np
+from profiles import users
 time = 24
 cap = 8
 #user[chargerate,maxcharge]
-users = [
 
-{"user":[5,50],"loadprof":[0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0],"demandprof": [(0.4,1),(0.6,0.9)],"count":0,"soc":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},  #user = [maxrate,maxcapacity]
-{"user":[4,70],"loadprof":[1,0,0,1,0,0,0,0,0,0,1,1,1,1,1,0,0,0,1,1,1,0,1,1],"demandprof": [(0.8,1),(0.1,0.9),(0.6,1),(0.4,1),(0.5,1)],"count":0,"soc":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}  #demandprof = (aantal laadbeurten, SOC beurt, SOC beurt,....)
-]
 timeload = []
 
 def dumpProfile(userlist,timesteps,cap):
@@ -16,12 +13,16 @@ def dumpProfile(userlist,timesteps,cap):
 
     for t in range(timesteps):
         print(t)
-        loads = [user.get('loadprof')[t]*user.get('user')[0] for user in userlist]
-        ls = sum(loads)
-        totals.append(ls)
-        if ls > cap:
-            diff = ls - cap
-            dep = diff/len(userlist)
+        # loads = []
+        # for user in userlist:
+        #     if user.get('soc')[t-1] == user.get('user')[1]:
+        #         loads.append(0) 
+        #     elif user.get('soc')[t-1] != user.get('user')[1]:
+        #         loads.append(user.get('loadprof')[t]*user.get('user')[0])
+
+            
+
+
 
 
         for user in userlist:
@@ -38,7 +39,6 @@ def dumpProfile(userlist,timesteps,cap):
 
             elif loadprof[t] == 1 and soc[t] != user.get('user')[1]:
 
-                if ls < cap:
                     if loadprof[t-1] == 0:
                         soc[t] = demand[0]*user.get('user')[1] + user.get('user')[0]
                         if soc[t] >= user.get('user')[1]:
@@ -59,29 +59,8 @@ def dumpProfile(userlist,timesteps,cap):
                     # if user.get('loadprof')[t+1] == 0:
                     #     n = user.get('count') + 1
                     #     user.update({'count':n})
-                
-                elif ls > cap:
-                    
+  
 
-                    if loadprof[t-1] == 0:
-                        soc[t] = demand[0]*user.get('user')[1] + (user.get('user')[0]-dep)
-                        if soc[t] >= user.get('user')[1]:
-                            profile.append(user.get('user')[1]- soc[t-1])
-                            soc[t] = user.get('user')[1]
-                        else:
-                            profile.append(user.get('user')[0]-dep)  
-                                              
-                    elif loadprof[t-1] == 1:
-                        soc[t]= soc[t-1] + (user.get('user')[0]-dep)
-                        if soc[t] >= user.get('user')[1]:
-                            profile.append(user.get('user')[1]- soc[t-1])
-                            soc[t] = user.get('user')[1]
-                        else:
-                            profile.append(user.get('user')[0]-dep)  
-
-                    # if user.get('loadprof')[t+1] == 0:
-                    #     n = user.get('count') + 1
-                    #     user.update({'count':n})
                 
             elif loadprof[t] == 1 and soc[t] == user.get("user")[1]:
                 profile.append(0)
@@ -89,6 +68,12 @@ def dumpProfile(userlist,timesteps,cap):
             
             else:
                 print("fout in verbruikverdeling")
+            
+
+
+
+            
+            
             if t < (len(loadprof)-1):
                 if loadprof[t] == 1 and loadprof[t+1] == 0:
                     if (count+1) < len(user.get('demandprof')):
@@ -97,8 +82,18 @@ def dumpProfile(userlist,timesteps,cap):
                     elif (count+1) == len(user.get('demandprof')):
                         new = count
                         user.update({"count":new})
-
             user.update({"soc":soc})
+        
+        loads = [prof[t] for prof in newprofs ]
+        ls = sum(loads)
+        totals.append(ls)
+        if ls > cap:
+            diff = ls - cap
+            dep = diff/len(userlist)
+            for prof in newprofs:
+                prof[t] = prof[t] - dep
+                userlist[newprofs.index(prof)].get('soc')[t] = userlist[newprofs.index(prof)].get('soc')[t]-dep
+           
 
 
     profiles = {"cap":[cap]* timesteps,"sum":totals,"profiles":newprofs,"timevalues":timeload}
@@ -124,7 +119,7 @@ def dumpProfile(userlist,timesteps,cap):
     #general plot
     t = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
     plt.plot(profiles.get("cap"),linestyle="dashed",label="limiet")
-    plt.step(t,profiles.get("sum"),linestyle="dashed",label="som",where="post")
+    plt.step(t,profiles.get("sum"),linestyle="dashed",label="potentiele som",where="post")
     for profile in profiles.get('profiles'):
         plt.step(t,profile,label=str("laadprofiel user"+str(profiles.get('profiles').index(profile) +1)),where="post")
     # plt.step(t,users[0].get('loadprof'))
