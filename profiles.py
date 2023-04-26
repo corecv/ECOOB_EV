156a963ab44485f84718aa6a4c9e87084660ecec
Pierre Magnus <pitou.magnus@hotmail.be>
2023-04-26 10:03:47 +0200

    profiles.py in kwartier

--- profiles.py
+++ profiles.py
@@ -1,10 +1,16 @@
-
-
+load1 = [0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0]
+load2 = [1,0,0,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,0,1,1]
+load1 = [val for val in load1 for _ in (0, 1, 2, 4)]
+load2 = [val for val in load2 for _ in (0, 1, 2, 4)]
+soc1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+soc2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+soc1 = [val for val in soc1 for _ in (0, 1, 2, 4)]
+soc2 = [val for val in soc2 for _ in (0, 1, 2, 4)]
 
 
 
 users = [
 
-{"user":[5,50],"loadprof":[0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,0,0,0,0],"demandprof": [(0.4,1),(0.6,0.9)],"count":0,"soc":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},  #user = [maxrate,maxcapacity]
-{"user":[4,70],"loadprof":[1,0,0,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,0,1,1],"demandprof": [(0.5,1),(0.1,0.9),(0.6,1),(0.4,1),(0.5,1)],"count":0,"soc":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}  #demandprof = (aantal laadbeurten, SOC beurt, SOC beurt,....)
+{"user":[5,50],"loadprof":load1,"demandprof": [(0.4,1),(0.6,0.9)],"count":0,"soc":soc1},  #user = [maxrate,maxcapacity]
+{"user":[4,70],"loadprof":load2,"demandprof": [(0.5,1),(0.1,0.9),(0.6,1),(0.4,1),(0.5,1)],"count":0,"soc":soc2}  #demandprof = (aantal laadbeurten, SOC beurt, SOC beurt,....)
 ]
