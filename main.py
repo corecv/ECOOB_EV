fb17ca3494df0d5f29891f51f0283f2ec4273232
Pierre Magnus <pitou.magnus@hotmail.be>
2023-05-26 16:38:59 +0200

    winkels juist

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ from datetime import datetime
 
 
 #gebruikers van het type 1
-nb_users_type1_no_priority = 10
-nb_users_type1_priority =7
+nb_users_type1_no_priority = 1
+nb_users_type1_priority =1
 
 #gebruikers van het type 2
-nb_users_type2_no_priority = 5
-nb_users_type2_priority = 4
+nb_users_type2_no_priority = 1
+nb_users_type2_priority = 1
 
 #gebruikers van het type 3
-nb_users_type3_no_priority = 10
-nb_users_type3_priority = 5
+nb_users_type3_no_priority = 1
+nb_users_type3_priority = 1
 
 #gebruikers van het type 4
-nb_users_type4_no_priority = 10
-nb_users_type4_priority = 10
+nb_users_type4_no_priority = 1
+nb_users_type4_priority = 1
 
 #gebruikers van het type 5
-nb_users_type5_no_priority =15
-nb_users_type5_priority = 5
+nb_users_type5_no_priority = 1
+nb_users_type5_priority = 1
 
 #gebruikers van het type 6
-nb_users_type6_no_priority = 10
-nb_users_type6_priority = 5
+nb_users_type6_no_priority = 1
+nb_users_type6_priority = 1
 
 #gebruikers van het type 7
-nb_users_type7_priority =4
+nb_users_type7_priority = 1
 
 
 #input gegevens van het gebouw 
